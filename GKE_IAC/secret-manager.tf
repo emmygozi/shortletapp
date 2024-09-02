d9d92047c58cc66a2ae591380c07421c559ad39c
emmygozi <mygitmail@yahoo.com>
2024-09-02 22:01:47 +0100

    final update to readme

--- GKE_IAC/secret-manager.tf
+++
@@ -1,9 +0,0 @@
-# # Retrieve the latest version of the secret
-# data "google_secret_manager_secret_version" "my_secret" {
-#   secret = "shortlet"
-# }
-
-# # Use the secret's value
-# output "my_secret_value" {
-#   value = data.google_secret_manager_secret_version.my_secret.secret_data
-# }
